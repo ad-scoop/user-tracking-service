fd2df48c84e1a0c784e28a1a1ad48941ed2b734a
= <=>
2017-02-20 22:07:18 +0100

    inital commit

---
+++ src/main/java/com/adscoop/usertrackingservice/StartUserTrackingService.java
@@ -0,0 +1,13 @@
+package com.adscoop.usertrackingservice;
+
+/**
+ * Created by thokle on 20/02/2017.
+ */
+public class StartUserTrackingService  {
+
+    public static void main(String... args){
+
+
+    }
+
+}
